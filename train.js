5c569889f79c001fdb05adb8cc92748ee250f4b1
Abdulmukhtor <Abdulmukhtor18@gmail.com>
2025-09-22 16:43:51 +0900

    feat: masala ishlandi

--- train.js
+++ train.js
@@ -1,12 +1,12 @@
-console.log('Jack Ma maslahatlari');
-const list = [
-     "yaxshi talab bo'ling",
-     "to'g'ri boshliq tanlang,ko'p xatolar qiling",
-     "o'zingizga ishlashni boshlang",
-     "siz kuchli bo'lgan narsalarni qiling",
-     "yoshlarga investitsiya qiling",
-     "endi dam oling,foydasi yo'q"
-];
+// console.log('Jack Ma maslahatlari');
+// const list = [
+//      "yaxshi talab bo'ling",
+//      "to'g'ri boshliq tanlang,ko'p xatolar qiling",
+//      "o'zingizga ishlashni boshlang",
+//      "siz kuchli bo'lgan narsalarni qiling",
+//      "yoshlarga investitsiya qiling",
+//      "endi dam oling,foydasi yo'q"
+// ];
 
 // function maslahatBering(a,callback){
 //     if (typeof a!== "number") callback("insert a number",null);
@@ -30,22 +30,22 @@ const list = [
 // console.log("passed here 1");
 
 
- async function maslahatBering(a){
-    if (typeof a!== "number") throw new Error("insert a number");
-    else if (a<=20) return list[0];
-    else if (a>20 && a<=30) return list[1];
-    else if (a>30 && a<=40) return list[2];
-    else if (a>40 && a<=50) return list[3];
-    else if (a>50 && a<=60) return list[4];
-    else{
-        return new Promise((resolve,reject)=>{
-            setTimeout(function(){
-                resolve(list[5]);
-            },5000);
-        })
+//  async function maslahatBering(a){
+//     if (typeof a!== "number") throw new Error("insert a number");
+//     else if (a<=20) return list[0];
+//     else if (a>20 && a<=30) return list[1];
+//     else if (a>30 && a<=40) return list[2];
+//     else if (a>40 && a<=50) return list[3];
+//     else if (a>50 && a<=60) return list[4];
+//     else{
+//         return new Promise((resolve,reject)=>{
+//             setTimeout(function(){
+//                 resolve(list[5]);
+//             },5000);
+//         })
        
-    }
-}
+//     }
+// }
 
 // .then/.catch
 // console.log("passed here 0")
@@ -59,12 +59,27 @@ const list = [
 // console.log("passed here 1");
 
 
-async function run(){
-    let javob = await maslahatBering(20);
-    console.log(javob);
-    javob = await maslahatBering(70);
-    console.log(javob);
-    javob = await maslahatBering(41);
-    console.log(javob);
+// async function run(){
+//     let javob = await maslahatBering(20);
+//     console.log(javob);
+//     javob = await maslahatBering(70);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+// }
+// run();
+
+// A-task
+function countLetter(letter, word) {
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      count++;
+    }
+  }
+  return count;
 }
-run();
+
+
+console.log(countLetter("e", "engineer")); 
+console.log(countLetter("a", "mexanizalashtirolmaganingizdandurda")); 
