3e5d9d9b9d0dec934bdc3fb50aa9c0ead7af5c4d
Abdulmukhtor <Abdulmukhtor18@gmail.com>
2025-09-29 00:25:19 +0900

    feat:Crud operations

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+
+const fs = require("fs")
+
+let user;
+fs.readFile("database/user.json","utf8",(err,data)=>{
+    if(err){
+        console.log("ERROR:",err);
+    }else{
+        user = JSON.parse(data)
+    }
+})
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// Routes
+
+app.post("/create-item",(req,res)=>{
+    console.log(req.body);
+    const new_reja=req.body.reja;
+    db.collection("plans").insertOne({reja:new_reja},(err,data)=>{
+        if(err){
+            console.log(err);
+            res.end("something one wrong");
+        }else{
+            res.end("succesfully added");
+        }
+    });
+});
+
+
+app.get('/', function(req, res) {
+    console.log('user entered /');
+    db.collection("plans")
+    .find()
+    .toArray((err,data)=>{
+        if(err){
+            console.log(err);
+            res.end("something one wrong");
+        }else{
+            console.log(data);
+            res.render("reja");
+        }
+    });
+    res.render('reja'); 
+});
+// app.get('/author',(req,res)=>{
+//     res.render("author",{user:user});
+// });
+
+// Server
--- server.js
+++ server.js
@@ -1,43 +1,20 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const fs = require("fs")
+const http = require("http");
+const { MongoClient } = require("mongodb");
+const app = require("./app");
 
-let user;
-fs.readFile("database/user.json","utf8",(err,data)=>{
-    if(err){
-        console.log("ERROR:",err);
-    }else{
-        user = JSON.parse(data)
-    }
-})
+const connectionString = "mongodb+srv://abdulmukhtor18_db:J0WjbMsUBsAjsf0k@cluster0.kyhaujq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// MongoDB ga ulanish
+MongoClient.connect(connectionString)
+  .then(client => {
+    console.log("✅ MongoDB connected successfully");
 
-// View engine
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-// Routes
-
-app.post("/create-item",(req,res)=>{
-    console.log(req.body);
-    res.json({test:"success"});
-});
-
-// app.get('/', function(req, res) {
-//     res.render('harid'); // views/harid.ejs
-// });
-
-app.get('/author',(req,res)=>{
-    res.render("author",{user:user});
-});
-
-// Server
-const server = http.createServer(app);
-const PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`The server is running successfully on port ${PORT}`);
-});
+    const server = http.createServer(app);
+    const PORT = 3000;
+    server.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error("MongoDB connection error:", err);
+  });
